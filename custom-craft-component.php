51b0268e7ec689bea40ccacedc88ea993a4b6ac5
Abhishek <abhishektk18@gmail.com>
2025-05-03 10:59:51 +0530

    Version update 1.8

--- custom-craft-component.php
+++ custom-craft-component.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Craft Component
  * Description: Create custom frontend components with fields like text and textareas.
- * Version: 1.1.7
+ * Version: 1.1.8
  * Author: Abhishek
  */
 
